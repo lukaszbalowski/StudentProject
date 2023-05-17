104c847a1b131d3184d00c2d42abf503cabba9bb
lukaszbalowski <l.balowski@gmail.com>
2023-05-17 19:56:52 +0200

    pierwszy commit

---
+++ src/test/java/pl/coderslab/studentsproject/StudentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.studentsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
